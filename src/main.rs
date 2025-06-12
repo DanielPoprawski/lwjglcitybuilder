1785dc7527dce2a1d329ce26286046296af5a2f5
DanielPoprawski <danielbpoprawski@gmail.com>
2025-06-12 13:54:27 -0400

    Added cursor capture and speed to player camera

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,12 @@ use std::f32::consts::PI;
 
 use bevy::{
     color::palettes::css::*,
+    core_pipeline::bloom::Bloom,
     diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin},
     input::mouse::MouseMotion,
+    pbr::CascadeShadowConfigBuilder,
     prelude::*,
+    window::CursorGrabMode,
 };
 
 const WINDOW_WIDTH: f32 = 1920.0;
@@ -15,7 +18,7 @@ fn main() {
             primary_window: Some(Window {
                 title: "My City Builder".into(),
                 resolution: (WINDOW_WIDTH, WINDOW_HEIGHT).into(),
-                // present_mode: bevy::window::PresentMode::Immediate,
+                present_mode: bevy::window::PresentMode::AutoVsync,
                 ..default()
             }),
             ..Default::default()
@@ -23,7 +26,7 @@ fn main() {
         .add_systems(Startup, startup)
         .add_systems(Update, update)
         .add_systems(Update, handle_input)
-        .add_systems(Update, handle_light)
+        // .add_systems(Update, handle_light)
         .add_plugins(FrameTimeDiagnosticsPlugin::default())
         .add_plugins(LogDiagnosticsPlugin::default())
         .run();
@@ -34,6 +37,9 @@ struct CameraController {
     pitch: f32,
     yaw: f32,
     sensitivity: f32,
+    velocity: Vec2,
+    smoothing: f32,
+    speed: f32,
 }
 
 fn startup(mut commands: Commands, asset_server: Res<AssetServer>) {
@@ -44,7 +50,18 @@ fn startup(mut commands: Commands, asset_server: Res<AssetServer>) {
         CameraController {
             pitch: 0.0,
             yaw: 0.0,
-            sensitivity: 0.004,
+            sensitivity: 0.002,
+            velocity: Vec2::ZERO,
+            smoothing: 0.15,
+            speed: 5.0,
+        },
+        Bloom {
+            intensity: 0.3,
+            low_frequency_boost: 0.7,
+            low_frequency_boost_curvature: 0.95,
+            high_pass_frequency: 1.0,
+            composite_mode: bevy::core_pipeline::bloom::BloomCompositeMode::Additive,
+            ..default()
         },
     ));
     commands.spawn(SceneRoot(
@@ -52,7 +69,7 @@ fn startup(mut commands: Commands, asset_server: Res<AssetServer>) {
     ));
     commands.insert_resource(AmbientLight {
         color: WHITE.into(),
-        brightness: 0.,
+        brightness: 10.,
         ..default()
     });
     commands.spawn((
@@ -63,7 +80,7 @@ fn startup(mut commands: Commands, asset_server: Res<AssetServer>) {
         //     ..default()
         // },
         DirectionalLight {
-            illuminance: light_consts::lux::DIRECT_SUNLIGHT,
+            illuminance: light_consts::lux::OVERCAST_DAY,
             shadows_enabled: true,
             ..default()
         },
@@ -72,61 +89,69 @@ fn startup(mut commands: Commands, asset_server: Res<AssetServer>) {
             rotation: Quat::from_rotation_x(-PI / 4.),
             ..default()
         },
+        CascadeShadowConfigBuilder {
+            first_cascade_far_bound: 500.0,
+            ..default()
+        }
+        .build(),
     ));
 }
 fn update() {}
 
-fn handle_light(
-    keyboard_input: Res<ButtonInput<KeyCode>>,
-    mut query: Query<&mut Transform, With<DirectionalLight>>,
-    mut counter: Local<f32>,
-) {
-    for mut transform in query {
-        if keyboard_input.pressed(KeyCode::ArrowUp) {
-            // light.brightness += 0.01;
-            *counter += 0.1;
-        }
-        if keyboard_input.pressed(KeyCode::ArrowDown) {
-            // light.brightness -= 0.01;
-            // transform.translation.y -= 1.;
-            *counter -= 0.1;
-        }
-        transform.rotation = Quat::from_rotation_x((-PI * (*counter / 128.)) / 4.)
-    }
-}
+// fn handle_light(
+//     keyboard_input: Res<ButtonInput<KeyCode>>,
+//     query: Query<&mut CameraController>,
+//     mut counter: Local<f32>,
+// ) {
+//     for mut camera in query {
+//         if *counter == 0.0 {
+//             *counter = 0.85;
+//         }
+//         if keyboard_input.just_pressed(KeyCode::ArrowUp) {
+//             *counter += 0.05;
+//         }
+//         if keyboard_input.just_pressed(KeyCode::ArrowDown) {
+//             *counter -= 0.05;
+//         }
+//         *counter = counter.clamp(0.01, 0.9999);
+//         // camera.smoothing = *counter;
+//         // println!("{}", camera.smoothing);
+//     }
+// }
 
 fn handle_input(
     keyboard_input: Res<ButtonInput<KeyCode>>,
     mouse_input: Res<ButtonInput<MouseButton>>,
     mut mouse_movement: EventReader<MouseMotion>,
     mut query: Query<(&mut Transform, &mut CameraController), With<Camera3d>>,
-    mut light: ResMut<AmbientLight>,
+    // light: ResMut<AmbientLight>,
     time: Res<Time>,
+    mut windows: Query<&mut Window>,
 ) {
     for (mut transform, mut camera_controller) in query.iter_mut() {
         if keyboard_input.pressed(KeyCode::KeyW) {
             let forward: Vec3 = *transform.forward();
-            transform.translation += forward * time.delta_secs();
+            transform.translation += forward * time.delta_secs() * camera_controller.speed;
         }
         if keyboard_input.pressed(KeyCode::KeyA) {
             let left: Vec3 = *transform.left();
-            transform.translation += left * time.delta_secs();
+            transform.translation += left * time.delta_secs() * camera_controller.speed;
         }
         if keyboard_input.pressed(KeyCode::KeyS) {
             let back: Vec3 = *transform.back();
-            transform.translation += back * time.delta_secs();
+            transform.translation += back * time.delta_secs() * camera_controller.speed;
         }
         if keyboard_input.pressed(KeyCode::KeyD) {
             let right: Vec3 = *transform.right();
-            transform.translation += right * time.delta_secs();
+            transform.translation += right * time.delta_secs() * camera_controller.speed;
         }
         if keyboard_input.pressed(KeyCode::Space) {
             let up: Vec3 = *transform.up();
-            transform.translation += up * time.delta_secs();
+            transform.translation += up * time.delta_secs() * camera_controller.speed;
         }
         if keyboard_input.pressed(KeyCode::ShiftLeft) {
             let down: Vec3 = *transform.down();
-            transform.translation += down * time.delta_secs();
+            transform.translation += down * time.delta_secs() * camera_controller.speed;
         }
         if keyboard_input.pressed(KeyCode::ArrowUp) {
             // light.brightness += 0.01;
@@ -134,23 +159,29 @@ fn handle_input(
         if keyboard_input.pressed(KeyCode::ArrowDown) {
             // light.brightness -= 0.01;
         }
-        for &event in mouse_movement.read() {
-            if mouse_input.pressed(MouseButton::Right) {
-                camera_controller.yaw -= event.delta.x * camera_controller.sensitivity;
-                camera_controller.pitch -= event.delta.y * camera_controller.sensitivity;
-                // transform.rotation = Quat::from_euler(
-                //     EulerRot::XYZ,
-                //     event.delta.x * 0.04,
-                //     event.delta.y * 0.04,
-                //     0.,
-                // );
+        if mouse_input.pressed(MouseButton::Right) {
+            if let Ok(mut window) = windows.single_mut() {
+                window.cursor_options.grab_mode = CursorGrabMode::Locked;
+                window.cursor_options.visible = false;
+            }
+            let mut cumulative_movement = Vec2::ZERO;
+            for event in mouse_movement.read() {
+                cumulative_movement += event.delta;
+            }
+            camera_controller.velocity = camera_controller.velocity * camera_controller.smoothing
+                + cumulative_movement * (1.0 - camera_controller.smoothing);
+            camera_controller.yaw -= camera_controller.velocity.x * camera_controller.sensitivity;
+            camera_controller.pitch -= camera_controller.velocity.y * camera_controller.sensitivity;
+        } else {
+            mouse_movement.clear();
+            camera_controller.velocity *= 0.2;
+            if let Ok(mut window) = windows.single_mut() {
+                window.cursor_options.grab_mode = CursorGrabMode::None;
+                window.cursor_options.visible = true;
             }
         }
-        transform.rotation = Quat::from_euler(
-            EulerRot::XYZ,
-            camera_controller.pitch,
-            camera_controller.yaw,
-            0.,
-        );
+        let yaw_quat = Quat::from_rotation_y(camera_controller.yaw);
+        let pitch_quat = Quat::from_rotation_x(camera_controller.pitch);
+        transform.rotation = yaw_quat * pitch_quat;
     }
 }
